c4bc2b384e14e9deed7673ff65f396c77f0b471f
kamimura <kamimura_5336@yahoo.co.jp>
2019-03-02 06:35:27 +0900

    [add] storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: require.resolve('awesome-typescript-loader'),
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx'],
+    },
+};
---
+++ src/__stories__/index.tsx
@@ -0,0 +1,4 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+storiesOf('text', module).add('with text', () => <h1>text</h1>);
