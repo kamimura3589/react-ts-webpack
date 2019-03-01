71eb6bc9b1e646c0e67598c5b856b73a78150293
kamimura <kamimura_5336@yahoo.co.jp>
2019-03-01 17:05:24 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-export interface InterfaceAppProps { testMessage: string };
+import React from 'react';
+
+export interface InterfaceAppProps {
+    testMessage: string;
+}
+
 class App extends React.Component<InterfaceAppProps, undefined> {
-  render() {
-    return (
-      <div>
-        {this.props.testMessage}
-      </div>
-    );
-  }
+    render() {
+        return <div>{this.props.testMessage}</div>;
+    }
 }
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(
-  <App testMessage="Hello ts-react-jest app" />,
-  document.getElementById("app"),
-);
+ReactDOM.render(<App testMessage="Hello ts-react-jest app" />, document.getElementById('app'));
