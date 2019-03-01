c00673bb3f409ff134e474bb4e24dbbd53a65fe0
kamimura <kamimura_5336@yahoo.co.jp>
2019-03-02 07:42:04 +0900

    [fix] webpack-dev-server

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App testMessage="Hello ts-react-jest app" />, document.getElementById('app'));
+ReactDOM.render(<App testMessage="Hello ts-react-jest app" />, document.getElementById('root'));
