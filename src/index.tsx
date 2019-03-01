8877ed70f66ab780c8500553be0ca7a45b69cb16
kamimura <kamimura_5336@yahoo.co.jp>
2019-03-02 08:14:14 +0900

    [add] redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { reducer } from './reducers';
 import App from './App';
 
-ReactDOM.render(<App testMessage="Hello ts-react-jest app" />, document.getElementById('root'));
+const store = createStore(reducer, {}, composeWithDevTools());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App testMessage="Hello ts-react-jest app" />
+    </Provider>,
+    document.getElementById('root'),
+);
