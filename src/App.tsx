d7d405bc8c2026eae3391e91c0cc8ae4af17435b
kamimura <kamimura_5336@yahoo.co.jp>
2019-03-01 16:34:57 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+export interface InterfaceAppProps { testMessage: string };
+class App extends React.Component<InterfaceAppProps, undefined> {
+  render() {
+    return (
+      <div>
+        {this.props.testMessage}
+      </div>
+    );
+  }
+}
+export default App;
